d12c5ba6523ad70824876b328f612ae11eb7ba1f
madkilin <mad.kilin@gmail.com>
2024-01-08 02:40:02 +0700

    feat: dinamis variable with dotenv

--- config/Database.js
+++ config/Database.js
@@ -1,8 +1,14 @@
 import { Sequelize } from "sequelize";
+import dotenv from "dotenv";
+dotenv.config();
 
-const db = new Sequelize("db_gudang", "root", "", {
-    host: "localhost",
-    dialect: "mysql"
-})
+const db = new Sequelize({
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  username: process.env.DB_USERNAME,
+  password: process.env.DB_PW,
+  database: process.env.DB_NAME,
+  dialect: "mysql",
+});
 
 export default db;
